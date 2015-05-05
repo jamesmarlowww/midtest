2438fc152da3f14a5e2b1725b19b8e9eda7a15e3
james <jamesmarlowww@gmail.com>
2015-05-06 11:49:14 +1200

    fasdfasf

--- src/TernaryTree.java
+++ src/TernaryTree.java
@@ -8,34 +8,34 @@ import static java.lang.Math.max;
 //Each node contains references to its left, center and right children
 //as well as a reference to its parent
 
-public class TernaryTree<E>{
+public class TernaryTree<E> {
 
-	protected E val;
-	protected TernaryTree<E> parent;
-	protected TernaryTree<E> left, center, right;
+    protected E val;
+    protected TernaryTree<E> parent;
+    protected TernaryTree<E> left, center, right;
 
-	//Create an empty node (i.e. sentinel)
-	public TernaryTree(){
-		val=null;
+    //Create an empty node (i.e. sentinel)
+    public TernaryTree() {
+        val = null;
         parent = null;
-		left=right=center=this;
-	}
+        left = right = center = this;
+    }
 
-	//Create a leaf
-	public TernaryTree(E value){
-    //// Write your code below
+    //Create a leaf
+    public TernaryTree(E value) {
+        //// Write your code below
         val = value;
         right = center = left = new TernaryTree<>();
         setLeft(left);
         setCenter(center);
         setRight(right);
 
-    //// Write your code above
-	}
+        //// Write your code above
+    }
 
-	//Create an internal node
-	public TernaryTree(E value, TernaryTree<E> left, TernaryTree<E> center, TernaryTree<E> right){
-    //// Write your code below
+    //Create an internal node
+    public TernaryTree(E value, TernaryTree<E> left, TernaryTree<E> center, TernaryTree<E> right) {
+        //// Write your code below
         val = value;
         if (left == null) {
             left = new TernaryTree<>();
@@ -46,88 +46,90 @@ public class TernaryTree<E>{
         }
         setRight(right);
 
-        if(center == null) {
+        if (center == null) {
             center = new TernaryTree<>();
         }
         setCenter(center);
-    //// Write your code above
-	}
+        //// Write your code above
+    }
 
-	public E value(){
-		return val;
-	}
-	public TernaryTree<E> left(){
-		return left;
-	}
+    public E value() {
+        return val;
+    }
 
-	public TernaryTree<E> right(){
-		return right;
-	}
+    public TernaryTree<E> left() {
+        return left;
+    }
 
-	public TernaryTree<E> center(){
-		return center;
-	}
-	public TernaryTree<E> parent(){
-		return parent;
-	}
+    public TernaryTree<E> right() {
+        return right;
+    }
 
-	public boolean isEmpty(){
-		return val==null;
-	}
+    public TernaryTree<E> center() {
+        return center;
+    }
 
-	public void setLeft(TernaryTree<E> newLeft){
-		if(isEmpty()) return;
-		if(left!=null && left.parent()==this)
-			left.setParent(null);
-		left=newLeft;
-		left.setParent(this);
-	}
+    public TernaryTree<E> parent() {
+        return parent;
+    }
 
-	public void setCenter(TernaryTree<E> newCenter){
-		if(isEmpty()) return;
-		if(center!=null && center.parent()==this)
-			center.setParent(null);
-		center=newCenter;
-		center.setParent(this);
-	}
+    public boolean isEmpty() {
+        return val == null;
+    }
+
+    public void setLeft(TernaryTree<E> newLeft) {
+        if (isEmpty()) return;
+        if (left != null && left.parent() == this)
+            left.setParent(null);
+        left = newLeft;
+        left.setParent(this);
+    }
+
+    public void setCenter(TernaryTree<E> newCenter) {
+        if (isEmpty()) return;
+        if (center != null && center.parent() == this)
+            center.setParent(null);
+        center = newCenter;
+        center.setParent(this);
+    }
 
 
-	public void setRight(TernaryTree<E> newRight){
-		if(isEmpty()) return;
-		if(right!=null && right.parent()==this)
-			right.setParent(null);
-		right=newRight;
-		right.setParent(this);
-	}
+    public void setRight(TernaryTree<E> newRight) {
+        if (isEmpty()) return;
+        if (right != null && right.parent() == this)
+            right.setParent(null);
+        right = newRight;
+        right.setParent(this);
+    }
 
-	protected void setParent(TernaryTree<E> newParent){
-		if(!isEmpty()){
-			parent=newParent;
-		}
-	}
+    protected void setParent(TernaryTree<E> newParent) {
+        if (!isEmpty()) {
+            parent = newParent;
+        }
+    }
 
     //Return the height of the ternary tree
-	public int height(){
-    //// Write your code below
-        if(val == null) {
+    public int height() {
+        //// Write your code below
+        if (val == null) {
             return 0;
         } else {
             int max = Math.max(left.height(), center.height());
 //            return 1+Math.max(max,right.height());
             return max(max, right.height()) + 1;
         }
-    //// Write your code above
-	}
+        //// Write your code above
+    }
 
     //Return true if and only if this node is a leaf node (not an empty node)
-    public boolean isLeaf(){
-    //// Write your code below
+    public boolean isLeaf() {
+        //// Write your code below
         boolean result = false;
-        if(left()== null && center == null&& right == null) {
+        if (left.val == null && center.val == null && right.val == null) {
             result = true;
         }
         return result;
-    //// Write your code above
+        //// Write your code above
 
     }
 
@@ -136,61 +138,52 @@ public class TernaryTree<E>{
     *traversal following the order:
     *parent -> left tree -> center tree -> right tree
 	*/
-    public String toString(){
-    //// Write your code below
+    public String toString() {
+        //// Write your code below
 
         return leaves().toString();
 
-    //// Write your code above
-	}
+        //// Write your code above
+    }
 
 
     //Return the level of the node
-	public int level(){
-    //// Write your code below
-        TernaryTree<E> pointer = parent;
-        int count = 0;
-        while(pointer != null) {
-            count++;
-            pointer = pointer.parent;
+    public int level() {
+        //// Write your code below
+        if (parent() == null) {
+            return 0;
         }
-        return count;
-    //// Write your code above
-	}
+        return 1 + parent().level();
 
-    // Return an ArrayList storing the values of all leaves in the tree
-    public ArrayList<TernaryTree<E>> leaves(){
-    //// Write your code below
-        ArrayList<TernaryTree<E>> returnList = new ArrayList<TernaryTree<E>>();
-
-        if(this == null) {
-            System.out.println("problem hereeeee");
-            return returnList;
-        }
-
-        Stack<TernaryTree> stack = new Stack<TernaryTree>();
-        stack.push(this);
-
-        while(!stack.empty()){
-            TernaryTree n = stack.pop();
-            returnList.add(n);
-
-            if(n.right != null){
-                stack.push(n.right);
-            }
-            if(n.center!= null) {
-                stack.push(n.center);
-            }
-            if(n.left != null){
-                stack.push(n.left);
-            }
-        }
-
-        return returnList;
-    //// Write your code above
+        //// Write your code above
     }
 
-    public static void main(String[] args){
+    // Return an ArrayList storing the values of all leaves in the tree
+    public ArrayList<TernaryTree<E>> leaves() {
+        //// Write your code below
+        // ArrayList<TernaryTree<E>> returnList = new ArrayList<TernaryTree<E>>();
+        ArrayList<TernaryTree<E>> list = new ArrayList<>();
+
+        if (left.val != null) {
+            left.leaves();
+        }
+        if (center.val != null) {
+            center.leaves();
+        }
+        if (right.val != null) {
+            right.leaves();
+        }
+        if (left.val == null && right.val == null && center.val == null) {
+            list.add(new TernaryTree<E>(val));
+
+        }
+
+        return list;
+        //// Write your code above
+    }
+
+
+    public static void main(String[] args) {
         TernaryTree<String> a = new TernaryTree<String>("a");
         TernaryTree<String> b = new TernaryTree<String>("b");
         TernaryTree<String> c = new TernaryTree<String>("c");
@@ -218,17 +211,17 @@ public class TernaryTree<E>{
 
 
 
-       System.out.println("The tree rooted at a is "+a);
+        System.out.println("The tree rooted at a is " + a);
 
         System.out.println("The Leaves in this tree are ");
         ArrayList<TernaryTree<String>> leaves = a.leaves();
-        for(TernaryTree<String> t: leaves){
-            System.out.println("\t"+t.value()+" at level "+t.level());
+        for (TernaryTree<String> t : leaves) {
+            System.out.println("\t" + t.value() + " at level " + t.level());
         }
-        System.out.println("The height of the tree is "+a.height());
+        System.out.println("The height of the tree is " + a.height());
 
-        System.out.println(leaves.toString());
 
     }
+
 
 }
